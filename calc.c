80781cb5446dfc4e7cd7bd3fac8262e6457b014b
Ullas Pai <138370237+Ullaspai50@users.noreply.github.com>
2023-11-11 10:44:18 +0530

    Commited after a reset seperately

--- calc.c
+++ calc.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+#include<conio.h>
 void main(){
     int a,b,n;
     printf("Enter the numbers:");
@@ -8,16 +9,19 @@ void main(){
     printf("\nDifference of the values is %d",a-b);
     printf("\nMultiplication of the value is %d",a*b);
     printf("\nDivision of the value is %d",a/b);
-    printf("Enter the selection: ");
+    printf("\nEnter another number: ");
+    scanf("%d",&a);
+    printf("\nEnter the selection(1 - sin, 2 - cos and 3 - tan): ");
     scanf("%d",&n);
     switch(n){
-        case 1: printf("sin(a) = %f",sin(a));
+        case 1: printf("sin(%d) = %f",a,sin(a));
         break;
-        case 2: printf("cos(a) = %f",cos(a));
+        case 2: printf("cos(%d) = %f",a,cos(a));
         break;
-        case 3: printf("tan(a) = %f",tan(a));
+        case 3: printf("tan(%d) = %f",a,tan(a));
         break;
         default: printf("Invalid selection");
         break;
     }
+    getch();
 }
